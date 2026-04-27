df29896743c32c4a1b858337c628e268fd873ce8
Osanda G <osanda.gunasena@justice.gov.uk>
2026-04-27 15:29:15 +0100

    fix: remove console logs

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -10,13 +10,11 @@ const GUIDELINES_DIR = path.join(process.cwd(), 'content', 'guidelines');
  * Todo: move a more suitable place or rename this file.
  */
 export function getGuidelinePage(slug: string): { content: string } | null {
-  console.log('Attempting to load guideline content for slug:', slug);
   let filePath = path.join(GUIDELINES_DIR, `${slug}.mdx`);
   
   if (fs.existsSync(filePath)) {
     const raw = fs.readFileSync(filePath, 'utf-8');
     const { content } = matter(raw);
-    console.log('Loaded guideline content from', content);
     return { content };
   }
   filePath = path.join(GUIDELINES_DIR, `${slug}.md`);
@@ -24,7 +22,6 @@ export function getGuidelinePage(slug: string): { content: string } | null {
   if (fs.existsSync(filePath)) {
     const raw = fs.readFileSync(filePath, 'utf-8');
     const { content } = matter(raw);
-    console.log('Loaded guideline content from', content);
     return { content };
   }
 
